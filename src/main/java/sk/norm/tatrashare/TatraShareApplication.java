4588aee82df3ff91f6c994a4339c33b7b33e971f
kitproton <33626684+vilisvit@users.noreply.github.com>
2026-04-18 19:21:50 +0200

    add endpoints spec

--- src/main/java/sk/norm/tatrashare/TatraShareApplication.java
+++ src/main/java/sk/norm/tatrashare/TatraShareApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TatraShareApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TatraShareApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TatraShareApplication.class, args);
+    }
 
 }
