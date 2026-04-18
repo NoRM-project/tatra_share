f3ec8a520d1683d9628b7a1d77cf9d436be3d4ce
kitproton <33626684+vilisvit@users.noreply.github.com>
2026-04-18 15:55:56 +0200

    first commit

---
+++ src/main/java/sk/norm/tatrashare/TatraShareApplication.java
@@ -0,0 +1,13 @@
+package sk.norm.tatrashare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatraShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatraShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/norm/tatrashare/TatraShareApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.norm.tatrashare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatraShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
